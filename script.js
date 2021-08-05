8ecb4b6a5a045f4ce8c68b44237405bf71a88623
Sreejith02 <86353269+Sreejith02@users.noreply.github.com>
2021-08-05 21:24:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function doSomething(){
+
+let name=document.querySelector("#name").value;
+
+let city=document.querySelector("#city").value;
+  
+  document.querySelector("#sec").innerHTML=`<h1>Name:${name}
+City:${city}<h1>`;
+
+
+}
